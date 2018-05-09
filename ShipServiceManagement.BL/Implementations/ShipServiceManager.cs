c565cc1404bfba9410317a841819bd6225b607b4
Kevin Ly <kly1@avans.nl>
2018-05-09 12:07:11 +0200

    Added ShipServiceManager (logic)

---
+++ ShipServiceManagement.BL/Implementations/ShipServiceManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using ShipServiceManagement.Logic.Interfaces;
+using ShipServiceManagement.Messaging.Interfaces;
+using ShipServiceManagement.Models;
+using ShipServiceManagement.Persistence.Interfaces;
+using ShipServiceManagement.Messaging.Extensions;
+
+namespace ShipServiceManagement.Logic.Implementations
+{
+	public class ShipServiceManager : IShipServiceManager
+	{
+		private readonly IShipServiceService _shipServiceService;
+		private readonly IMessagePublisher _messagePublisher;
+
+		public ShipServiceManager(IShipServiceService shipServiceService, IMessagePublisher messagePublisher)
+		{
+			_shipServiceService = shipServiceService;
+			_messagePublisher = messagePublisher;
+		}
+
+		public async Task CreateShipService(ShipService shipService)
+		{
+			await _shipServiceService.AddAsync(shipService);
+			await _messagePublisher.PublishMessageAsync(MessageTypes.ServiceCreated, shipService);
+		}
+
+		public async Task DeleteShipService(string name)
+		{
+			await _shipServiceService.DeleteAsync(name);
+			await _messagePublisher.PublishMessageAsync(MessageTypes.ServiceDeleted, name);
+		}
+
+		public async Task UpdateShipService(ShipService shipService)
+		{
+			await _shipServiceService.UpdateAsync(shipService);
+			await _messagePublisher.PublishMessageAsync(MessageTypes.ServiceUpdated, shipService);
+		}
+	}
+}
---
+++ ShipServiceManagement.BL/Interfaces/IShipServiceManager.cs
@@ -0,0 +1,29 @@
+﻿using ShipServiceManagement.Models;
+using System.Threading.Tasks;
+
+namespace ShipServiceManagement.Logic.Interfaces
+{
+	public interface IShipServiceManager
+	{
+		/// <summary>
+		/// Creates the ship service.
+		/// </summary>
+		/// <param name="shipService">The ship service.</param>
+		/// <returns></returns>
+		Task CreateShipService(ShipService shipService);
+
+		/// <summary>
+		/// Updates the ship service.
+		/// </summary>
+		/// <param name="shipService">The ship service.</param>
+		/// <returns></returns>
+		Task UpdateShipService(ShipService shipService);
+
+		/// <summary>
+		/// Deletes the ship service.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		/// <returns></returns>
+		Task DeleteShipService(string name);
+	}
+}
